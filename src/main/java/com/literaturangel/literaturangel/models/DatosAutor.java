1f1736171f6a7f261ac4f00bca653907af18ea49
migeabr233 <migeabr9@gmail.com>
2024-07-13 03:56:25 -0400

    challenge literatura alura

---
+++ src/main/java/com/literaturangel/literaturangel/models/DatosAutor.java
@@ -0,0 +1,13 @@
+package com.literaturangel.literaturangel.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosAutor(
+
+        @JsonAlias("name") String nombre,
+        @JsonAlias("birth_year") int fechaDeNacimiento,
+        @JsonAlias("death_year") int fechaDeMuerte
+) {
+}
